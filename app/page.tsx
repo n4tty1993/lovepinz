29abc6dcd85bdfa18a3d26378728a8ce09c46693
Netanel Vaknin <emailofnetanel@gmail.com>
2026-02-20 23:13:36 +0200

    chore: add WhyMagnetic, CustomerExamples, PricingSection to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { HeroSection } from "@/components/home/HeroSection/HeroSection";
 import { TrustStrip } from "@/components/home/TrustStrip/TrustStrip";
 import { HowItWorks } from "@/components/home/HowItWorks/HowItWorks";
+import { WhyMagnetic } from "@/components/home/WhyMagnetic/WhyMagnetic";
+import { CustomerExamples } from "@/components/home/CustomerExamples/CustomerExamples";
+import { PricingSection } from "@/components/home/PricingSection/PricingSection";
 
 export default function Page() {
   return (
@@ -8,6 +11,9 @@ export default function Page() {
       <HeroSection />
       <TrustStrip />
       <HowItWorks />
+      <WhyMagnetic />
+      <CustomerExamples />
+      <PricingSection />
     </main>
   );
 }
