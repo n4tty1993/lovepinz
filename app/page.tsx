3af9e82ef18a5db2df644b7bca9e42e29fc8137b
Netanel Vaknin <emailofnetanel@gmail.com>
2026-02-20 23:16:35 +0200

    feat: assemble full homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { HowItWorks } from "@/components/home/HowItWorks/HowItWorks";
 import { WhyMagnetic } from "@/components/home/WhyMagnetic/WhyMagnetic";
 import { CustomerExamples } from "@/components/home/CustomerExamples/CustomerExamples";
 import { PricingSection } from "@/components/home/PricingSection/PricingSection";
+import { UseCases } from "@/components/home/UseCases/UseCases";
+import { FAQSection } from "@/components/home/FAQSection/FAQSection";
+import { FinalCTA } from "@/components/home/FinalCTA/FinalCTA";
 
 export default function Page() {
   return (
@@ -14,6 +17,9 @@ export default function Page() {
       <WhyMagnetic />
       <CustomerExamples />
       <PricingSection />
+      <UseCases />
+      <FAQSection />
+      <FinalCTA />
     </main>
   );
 }
